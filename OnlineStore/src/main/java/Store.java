827a0362733e1379f30a8d951c49a91db7fcf6fb
Emily Trifone <emilytrifone@gmail.com>
2024-04-26 16:13:27 -0400

    Completed checkOut

--- OnlineStore/src/main/java/Store.java
+++ OnlineStore/src/main/java/Store.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -19,7 +20,7 @@ public class Store {
 
     public static void main(String[] args) {
         // Populate the inventory
-        readFromFile("products.csv");
+        readFromFile();
         System.out.println("Welcome to The Online Store!");
         homeMenu();
     }
@@ -34,55 +35,94 @@ public class Store {
 
             int choice = scanner.nextInt();
 
+
             switch (choice) {
                 case 1:
                     displayProducts();
                     break;
                 case 2:
-                    // display cart
-                    if (!cart.getItems().isEmpty()) {
-                        System.out.println(cart.getItems());
-                        System.out.println("Would you like to Remove an Item? (Y/N)");
-                        scanner.nextLine();
-                        String remove = scanner.nextLine();
-                        if (remove.equalsIgnoreCase("y")) {
-                            System.out.println("Enter the name of the item you want to remove");
-                            String itemName = scanner.nextLine();
-                            for (Product item : inventory) {
-                                if (item.getName().equalsIgnoreCase(itemName)){
-                                    cart.removeFromCart(item);
-                                    System.out.println("Item removed");
-                                    break;
-                                }
-
-                            }
-                        }
-                    } else {
-                        System.out.println("You have no items in your cart");
-                        homeMenu();
-                        //continue;
-                    }
+                    displayCart();
                     break;
-
                 case 3:
                     scanner.close();
                     return;
 
             }
 
+        }
+    }
 
+    public static void displayCart() {
+        // display cart
+        if (!cart.getItems().isEmpty()) {
+            System.out.println(cart.getItems());
+            System.out.println(cart.getTotalPrice());
+
+            System.out.println("What would you like to do?\n\t" +
+                    "1) Remove Item\n\t" +
+                    "2) Check Out\n\t" +
+                    "3) Return to Main Menu");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    removeFromCart();
+                    break;
+                case 2:
+                    checkOut();
+                    break;
+                case 3:
+                    homeMenu();
+
+            }
+        } else {
+            System.out.println("You have no items in your cart");
+            homeMenu();
+            //continue;
+        }
+    }
+
+    public static void removeFromCart() {
+        System.out.println("Enter the name of the item you want to remove");
+        scanner.nextLine();
+        String itemName = scanner.nextLine();
+        for (Product item : inventory) {
+            if (item.getName().equalsIgnoreCase(itemName)) {
+                cart.removeFromCart(item);
+                System.out.println("Item removed");
+                break;
+            }
+        }
+    }
+
+    public static void checkOut() {
+        System.out.println("Sales total: $"+cart.getTotalPrice());
+
+        System.out.print("Please enter cash amount: $");
+        double payment = scanner.nextDouble();
+
+        if (payment >= cart.getTotalPrice()) {
+            double change = (payment-cart.getTotalPrice());
+            LocalDateTime purchaseDate = LocalDateTime.now();
+
+            System.out.println(purchaseDate+"\n"+cart.getItems()+"\n"+cart.getTotalPrice()+"\n"+payment+"\n"+change);
+            for (Product item : inventory){
+
+                cart.removeFromCart(item);
+            }
+            homeMenu();
         }
     }
 
     public static void displayProducts() {
         for (Product item : inventory) {
             System.out.println(item.toString());
-
         }
-        System.out.println(" ");
-        System.out.println("What would you like to do?");
-        System.out.println("\t1) Search or filter items\n\t" +
-                "2) Return to home screen");
+        System.out.println("\nWhat would you like to do?");
+        System.out.println("\t1) Search for items\n\t" +
+                "2) Filter items\n\t" +
+                "3) Return to home screen");
 
         int choice = scanner.nextInt();
 
@@ -91,6 +131,9 @@ public class Store {
                 searchInventory();
                 break;
             case 2:
+                filterInventory();
+                break;
+            case 3:
                 break;
         }
 
@@ -98,7 +141,7 @@ public class Store {
 
     public static void searchInventory() {
         scanner.nextLine();
-        System.out.print("Please enter the name of the item: ");
+        System.out.print("Choose item: ");
         String item = scanner.nextLine();
 
         for (Product product : inventory) {
@@ -116,46 +159,90 @@ public class Store {
         System.out.println("Item not found");
     }
 
+        public static void filterInventory() {
+            System.out.println("What would you like to filter by?\n\t" +
+                    "1) Price range\n\t" +
+                    "2) Department");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
-    public static void readFromFile(String filename) {
-        // Try to open the file
-        try {
-            BufferedReader bfr = new BufferedReader(new FileReader("products.csv"));
-            String input;
-            boolean skippedHeader = false;
-
-            while ((input = bfr.readLine()) != null) {
-                if (!skippedHeader) {
-                    skippedHeader = true;
-                    continue;
-                }
-                // Populate the CSV inventory
-                getCSVInventory(input);
+            if (choice == 1) {
+                filterByPriceRange();
+            } else if (choice == 2) {
+                filterByDepartment();
+            } else {
+                System.out.println("Invalid choice");
+                homeMenu();
             }
-            bfr.close();
+        }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        public static void filterByPriceRange() {
+            float minPrice;
+            float maxPrice;
+            System.out.println("Please enter the minimum price of the product: ");
+            minPrice = scanner.nextFloat();
+            System.out.println("Please enter the maximum price of the product: ");
+            maxPrice = scanner.nextFloat();
+            for (Product product : inventory) {
+                if (product.getPrice() >= minPrice && product.getPrice() <= maxPrice) {
+                    System.out.println(product);
+                }
+            }
+            searchInventory();
+        }
+
+        public static void filterByDepartment() {
+
+            System.out.println("Please enter the department you would like to filter by: ");
+            String department = scanner.nextLine();
+
+            for (Product item : inventory) {
+                if (item.getDepartment().equalsIgnoreCase(department)) {
+                    System.out.println(item.toString());
+                }
+            }
+            searchInventory();
+        }
+
+        public static void readFromFile (){
+            // Try to open the file
+            try {
+                BufferedReader bfr = new BufferedReader(new FileReader("products.csv"));
+                String input;
+                boolean skippedHeader = false;
+
+                while ((input = bfr.readLine()) != null) {
+                    if (!skippedHeader) {
+                        skippedHeader = true;
+                        continue;
+                    }
+                    // Populate the CSV inventory
+                    getCSVInventory(input);
+                }
+                bfr.close();
+
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        }
+
+        // Populate the CSV inventory
+        public static void getCSVInventory (String input){
+            // split the input
+            String[] tokens = input.split("\\|");
+
+            // Parse the tokens
+            String sku = tokens[0];
+            String name = tokens[1];
+            double price = Double.parseDouble(tokens[2]);
+            String department = tokens[3];
+
+            // Creates the product
+            Product product = new Product(sku, name, price, department);
+
+            // Adds the product to csvInventory
+            inventory.add(product);
         }
     }
-
-    // Populate the CSV inventory
-    public static void getCSVInventory(String input) {
-        // split the input
-        String[] tokens = input.split("\\|");
-
-        // Parse the tokens
-        String sku = tokens[0];
-        String name = tokens[1];
-        double price = Double.parseDouble(tokens[2]);
-        String department = tokens[3];
-
-        // Creates the product
-        Product product = new Product(sku, name, price, department);
-
-        // Adds the product to csvInventory
-        inventory.add(product);
-    }
-}
